e57e648d302edd5dfb5f35db3339b1ffa5968214
jongwooo <jongwooo.han@gmail.com>
2022-12-02 00:44:00 +0900

    chore: Create check-commit-messages.sh

---
+++ scripts/check-commit-messages.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+REGEX_NORMAL="(feat|fix|docs|style|refactor|test|chore)(\(.+\))?:.{1,50})"
+REGEX_MERGE="(Merge pull request #[[:digit:]]{1,10}( from .*/.*)?"
+REGEX="^(${REGEX_NORMAL}|${REGEX_MERGE})$"
+
+echo
+
+fail=$(git show -s --format=format:'%s' "$@" | grep -v -E "${REGEX}")
+
+echo "$fail"
+
+if [[ $fail ]]
+then
+    echo ""
+    echo "Above ↑ commits don't conform to commit message format:"
+    echo "https://github.com/jongwooo/gatsby-cache/blob/main/.github/CONTRIBUTING.md#commit-message-rules"
+    echo ""
+    echo "Please fix."
+    exit 1
+fi
