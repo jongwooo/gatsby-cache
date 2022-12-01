dbf6f9b89927b713fb426032179ddd80608438f0
jongwooo <jongwooo.han@gmail.com>
2022-12-02 01:21:17 +0900

    chore: Add new commit prefix types

--- scripts/check-commit-messages.sh
+++ scripts/check-commit-messages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REGEX_NORMAL="(feat|fix|docs|style|refactor|test|chore)(\(.+\))?:.{1,50})"
+REGEX_NORMAL="(feat|fix|docs|style|refactor|perf|test|chore|ci|revert)(\(.+\))?:.{1,68})"
 REGEX_MERGE="(Merge pull request #[[:digit:]]{1,10}( from .*/.*)?"
 REGEX="^(${REGEX_NORMAL}|${REGEX_MERGE})$"
 
