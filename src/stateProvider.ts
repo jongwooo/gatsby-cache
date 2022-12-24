62ca4d26109b3c6cb7acc9c6518d6191d86077ca
jongwooo <jongwooo.han@gmail.com>
2022-12-24 23:51:06 +0900

    feat: Create stateProvider.ts

---
+++ src/stateProvider.ts
@@ -0,0 +1,23 @@
+import * as core from "@actions/core";
+import { State } from "./constants";
+
+export interface BaseStateProvider {
+  getCacheState(): string | undefined;
+  getState(key: string): string;
+  setState(key: string, value: string): void;
+}
+
+export class StateProvider implements BaseStateProvider {
+  getCacheState(): string | undefined {
+    const cacheKey: string = this.getState(State.CacheMatchedKey);
+    if (cacheKey) {
+      core.debug(`Cache state/key: ${cacheKey}`);
+      return cacheKey;
+    }
+
+    return undefined;
+  }
+
+  getState = core.getState;
+  setState = core.saveState;
+}
