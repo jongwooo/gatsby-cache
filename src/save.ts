141f4556ccbcc72b460b4760e74713ff19c0a292
jongwooo <jongwooo.han@gmail.com>
2022-12-25 00:14:37 +0900

    feat: Implement save function

--- src/save.ts
+++ src/save.ts
@@ -1,42 +1,10 @@
-import * as cache from "@actions/cache";
-import * as core from "@actions/core";
-import * as utils from "./utils/actionUtils";
-import { State } from "./constants";
-
-process.on("uncaughtException", (error: unknown) =>
-  utils.logWarning((error as Error).message)
-);
+import saveImpl from "./saveImpl";
+import { StateProvider } from "./stateProvider";
 
 async function run(): Promise<void> {
-  try {
-    if (!utils.isCacheFeatureAvailable()) {
-      return;
-    }
-
-    const state: string | undefined = utils.getCacheState();
-
-    const primaryKey: string = core.getState(State.CachePrimaryKey);
-    if (!primaryKey) {
-      utils.logWarning(`Error retrieving key from state.`);
-      return;
-    }
-
-    if (utils.isExactKeyMatch(primaryKey, state)) {
-      core.info(
-        `Cache hit occurred on the primary key ${primaryKey}, not saving cache.`
-      );
-      return;
-    }
-
-    const cachePaths: string[] = await utils.getBuildOutputPaths();
-    const cacheId: number = await cache.saveCache(cachePaths, primaryKey);
-
-    if (cacheId != -1) {
-      core.info(`Cache saved with key: ${primaryKey}`);
-    }
-  } catch (error: unknown) {
-    utils.logWarning((error as Error).message);
-  }
+  await saveImpl(new StateProvider());
 }
 
 void run();
+
+export default run;
